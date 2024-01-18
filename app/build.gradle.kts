cd370da914ef3626afc885d7c22cb20f112857b8
gacmalony <gocmenmertkaan@gmail.com>
2024-01-18 21:09:52 +0100

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.mlkitapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.mlkitapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+
+}
+
+dependencies {
+        // To recognize Latin script
+        implementation ("com.google.mlkit:text-recognition:16.0.0")
+
+        // To recognize Chinese script
+        implementation ("com.google.mlkit:text-recognition-chinese:16.0.0")
+
+        // To recognize Devanagari script
+        implementation ("com.google.mlkit:text-recognition-devanagari:16.0.0")
+
+        // To recognize Japanese script
+        implementation ("com.google.mlkit:text-recognition-japanese:16.0.0")
+
+        // To recognize Korean script
+        implementation ("com.google.mlkit:text-recognition-korean:16.0.0")
+
+    implementation ("com.github.bumptech.glide:glide:4.14.2")
+    // Skip this if you don't want to use integration libraries or configure Glide.
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.14.2")
+
+    implementation ("com.google.android.gms:play-services-mlkit-text-recognition:19.0.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
